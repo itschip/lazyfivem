0f2ee9de6e5b97d702894fcde20dbcb00f199abd
itschip <onlygamernerd@gmail.com>
2022-09-11 23:50:06 +0200

    feat(gui): sidebar and main console window

--- cmd/lazyfivem/main.go
+++ cmd/lazyfivem/main.go
@@ -1,13 +1,88 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os/exec"
+	"sync"
 
 	"github.com/jroimartin/gocui"
 )
 
+var mu sync.Mutex
+
+func nextView(g *gocui.Gui, v *gocui.View) error {
+  if v == nil || v.Name() == "side" {
+    _, err := g.SetCurrentView("main")
+    return err
+  }
+
+  _, err := g.SetCurrentView("side")
+  return err
+}
+
+func cursorDown(g *gocui.Gui, v *gocui.View) error {
+  if v != nil {
+    cx, cy := v.Cursor()
+    if err := v.SetCursor(cx, cy+1); err != nil {
+      ox, oy := v.Origin()
+      if err := v.SetOrigin(ox, oy+1); err != nil {
+        return err
+      }
+    }
+  }
+
+  return nil
+}
+
+
+func cursorUp(g *gocui.Gui, v *gocui.View) error {
+  if v != nil {
+      ox, oy := v.Origin()
+      cx, cy := v.Cursor()
+      if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
+        if err := v.SetOrigin(ox, oy-1); err != nil {
+          return err
+        }
+      }
+  }
+
+  return nil
+}
+
+
+func keybinds(g *gocui.Gui) error {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+
+  // Next View
+	if err := g.SetKeybinding("main", gocui.KeyCtrlSpace, gocui.ModNone, nextView); err != nil {
+		return err
+	}
+
+  // Next View
+	if err := g.SetKeybinding("side", gocui.KeyCtrlSpace, gocui.ModNone, nextView); err != nil {
+		return err
+	}
+
+  // Cursor up
+	if err := g.SetKeybinding("side", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
+		return err
+	}
+
+  // Cursor down
+	if err := g.SetKeybinding("side", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
+		return err
+	}
+
+  return nil
+}
+
+
 func main() {
+
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		log.Panicln(err)
@@ -15,10 +90,28 @@ func main() {
 
 	defer g.Close()
 
+  g.Cursor = true
+
+  g.BgColor = gocui.ColorBlack
+  g.FgColor = gocui.ColorWhite
+  g.SelFgColor = gocui.ColorGreen
+
 	g.SetManagerFunc(layout)
 
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
+  if err := keybinds(g); err != nil {
+    log.Panicln(err)
+  }
+
+	cmd := exec.Command("X:/ServerFX/starter.bat")
+	r, _ := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
+	scanner := bufio.NewScanner(r)
+
+  go listen(g, scanner)
+	
+  err = cmd.Start()
+	if err != nil {
+		panic(err)
 	}
 
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
@@ -26,22 +119,52 @@ func main() {
 	}
 }
 
+
 func layout(g *gocui.Gui) error {
 
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("side", -1, -1, maxX/4, maxY); err != nil {
+	if s, err := g.SetView("side", 0, 0, maxX/4, maxY - 1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(v, "hello guy")
+
+    s.Title = "Servers"
+    s.FgColor = gocui.ColorWhite
+    s.SelFgColor = gocui.ColorGreen
+    s.Highlight = true
+    
+    fmt.Fprintln(s, "NPWD Server")
+		fmt.Fprintln(s, "ESX Server")
+		fmt.Fprintln(s, "QB Server")
+
+    if _, err := g.SetCurrentView("side"); err != nil {
+      return err
+    }
 	}
 
-	if v, err := g.SetView("main", int(0.2*float32(maxX)), -1, maxX, maxY); err != nil {
+
+	if v, err := g.SetView("main", int(0.2*float32(maxX)), 0, maxX - 1, maxY - 1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(v, "hello guy")
+
+    v.Title = "FXServer"
+    v.Autoscroll = true
+    v.Wrap = true
+    
+    fmt.Fprintln(v, "")
+
 	}
+
+  if c, err := g.SetView("command", int(0.2*float32(maxX)), maxY - 4, maxX - 1, maxY - 1); err != nil {
+    if err != gocui.ErrUnknownView {
+      return err
+    }
+
+
+    c.Title = "Command"
+    c.Editable = true
+  }
 
 	return nil
 }
@@ -49,3 +172,69 @@ func layout(g *gocui.Gui) error {
 func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
 }
+
+func listen(g *gocui.Gui, scanner *bufio.Scanner) {
+  for scanner.Scan() {
+    line := scanner.Text()
+
+    mu.Lock()
+    updateFxServer(g, line)
+  }
+}
+
+func updateFxServer(g *gocui.Gui, line string) {
+  g.Update(func(g *gocui.Gui) error {
+    v, err := g.View("main")
+    if err != nil {
+      return err
+    }
+
+    fmt.Fprintln(v, line)
+    mu.Unlock()
+    return nil
+  })
+}
+
+/*func fxServer(g *gocui.Gui) {
+	cmd := exec.Command("X:/ServerFX/starter.bat")
+	r, _ := cmd.StdoutPipe()
+	cmd.Stderr = cmd.Stdout
+
+  done := make(chan struct{})
+
+	scanner := bufio.NewScanner(r)
+
+
+  go func() {
+    for scanner.Scan() {
+      line := scanner.Text()
+     g.Update(func(g *gocui.Gui) error {
+        v, err := g.View("main")
+        if err != nil {
+          return err
+        }
+
+
+        v.Clear()
+        fmt.Fprintln(v, line)
+
+        return nil
+      })
+    }
+
+    done <- struct{}{}
+
+  }()
+
+	err := cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	<-done
+
+	err = cmd.Wait()
+	if err != nil {
+		panic(err)
+	}
+}*/
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/itschip/lazyfivem
 go 1.18
 
 require (
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/creack/pty v1.1.18 // indirect
 	github.com/jroimartin/gocui v0.5.0 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/nsf/termbox-go v1.1.1 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 )
